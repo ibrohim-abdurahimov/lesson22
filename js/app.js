ca2f6d31f02bc732dc6a4c1b0a575f271d9d745e
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-08-18 13:53:41 +0500

    js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-const deliver = document.querySelector(".navbarModel")
+const deliver = document.querySelector(".navbar__model")
 
 function closeModel(){
     deliver.style.display = "none"
+}
+function showModel(){
+    deliver.style.display = "block"
 }

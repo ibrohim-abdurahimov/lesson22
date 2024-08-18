2b176ea8b9b0c403082ce2e1b1000c2bb0537a1c
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-08-18 17:21:16 +0500

    hamburger

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 const deliver = document.querySelector(".navbar__model")
-
-function closeModel(){
-    deliver.style.display = "none"
-}
 function showModel(){
     deliver.style.display = "block"
+}
+function closeModel(){
+    deliver.style.display = "none"
+}   
+
+
+const all = document.querySelector(".menyu")
+function closeMenyu(){
+    all.style.display = "none"
+}
+function showMenyu(){
+    all.style.display = "block"
 }
